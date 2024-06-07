0d8eb0f9337ba01a5d73583907b2d38c0c7f2ef7
essabsh <essabsh@post.bgu.ac.il>
2024-06-07 19:35:00 +0300

    added instructions file and class diagram

--- dev/src/HR/Main.java
+++ dev/src/HR/Main.java
@@ -1,5 +1,6 @@
 package HR;
 
+import HR.DomainLayer.BranchPackage.Branch;
 import HR.DomainLayer.PersonnelManager;
 import HR.ServiceLayer.FactroyService;
 
@@ -13,6 +14,12 @@ public class Main {
     public static PersonnelManager manager = new PersonnelManager("Firas");
 
     public static void main(String[] args) throws Exception {
+        factroyService.addbranch(1,"Beer sheva");
+        factroyService.addEmployee(1,"essa",1,12000,1,"full","essa","essa12",15000,LocalDate.of(2023,8,2));
+        factroyService.addEmployee(2,"athel",2,12000,1,"full","athel","athel12",15000,LocalDate.of(2023,8,2));
+        factroyService.addEmployeeRole(1,"Shift Manager");
+        factroyService.addEmployeeRole(2,"cashier");
+        factroyService.addShift(1,LocalDate.of(2024,6,10),1,2,"Morning",1);
         MenuPage();
     }
 
